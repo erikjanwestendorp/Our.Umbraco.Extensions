0f4e2f779a6d1f5b1f358288bcf7fdad49d83303
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-02-15 14:49:14 +0100

    Add WebApplicationBuilderExtensions

---
+++ WebApplicationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Extensions;
+
+namespace Our.Umbraco.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+	public static IUmbracoBuilder CreateUmbracoBuilder(this WebApplicationBuilder builder, Action<WebApplicationBuilder> configure)
+	{
+		configure(builder);
+
+		return builder.CreateUmbracoBuilder();
+	}
+}
