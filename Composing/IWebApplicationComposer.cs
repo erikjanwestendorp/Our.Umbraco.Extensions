fa78910bb3227071a27430a71813d57cbfcc97e8
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2024-02-26 16:36:40 +0100

    Add IWebApplicationComposer

---
+++ Composing/IWebApplicationComposer.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Our.Umbraco.Extensions.Composing;
+
+public interface IWebApplicationComposer
+{
+    void Compose(WebApplicationBuilder builder);
+}
